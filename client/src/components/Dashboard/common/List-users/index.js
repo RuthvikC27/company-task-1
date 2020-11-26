5d298597aa57809605de09860e0fc022964fb586
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-11-26 23:15:02 +0530

    Initial

--- client/src/components/Dashboard/common/List-users/index.js
+++ client/src/components/Dashboard/common/List-users/index.js
@@ -2,28 +2,45 @@ import React, { useState, useEffect } from 'react';
 import Nav from '../Nav';
 import axios from 'axios';
 
+import classes from './index.module.css';
+
 const Listusers = ({ role, user }) => {
+    const [customers, setCustomers] = useState([]);
+    const [error, setError] = useState();
 
     useEffect(() => {
         axios.defaults.headers.common['Authorize'] = 'Bearer' + localStorage.getItem('token');
-                                
-        axios.get("/api/user/list-users", {
-            // headers: {
-            //     authorize: localStorage.getItem('token')
-            // }
-        })
-        .then(res => {
-            console.log(res.data);
-        })
-        .catch(err => {
-            console.log(err.response.data.errors)
-        })
+
+        axios.get("/api/user/list-users")
+            .then(res => {
+                setCustomers(res.data);
+            })
+            .catch(err => {
+                setError(err.response.data.errors[0].msg);
+            })
     }, [])
 
     return (
         <React.Fragment>
+            { error && <div className={classes.errorMsg}>{error}</div>}
             <Nav role={role}
                 user={user} />
+            <main>
+                <ul>
+                    {!error && <div className={classes.customerHeader}>
+                        <h1>Customers list</h1>
+                        <input className={classes.searchHeader} type="text" placeholder="Search user"/>
+                    </div>}
+                    {customers.length > 0 && customers.map(customer => {
+                        return <li key={customer._id}>
+                            <div className={classes.customerOne}>
+                                <p>User : {customer.username}</p>
+                                <p>Role : {customer.role}</p>
+                            </div>
+                        </li>
+                    })}
+                </ul>
+            </main>
         </React.Fragment>
     );
 };
