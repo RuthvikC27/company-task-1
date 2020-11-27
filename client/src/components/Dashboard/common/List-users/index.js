f63b50c4e764ecd9d42de8e14a0826565ee91f61
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-11-27 16:45:22 +0530

    New

--- client/src/components/Dashboard/common/List-users/index.js
+++ client/src/components/Dashboard/common/List-users/index.js
@@ -3,10 +3,12 @@ import Nav from '../Nav';
 import axios from 'axios';
 
 import classes from './index.module.css';
+import LoadingImg from './spinner.svg';
 
 const Listusers = ({ role, user }) => {
     const [customers, setCustomers] = useState([]);
     const [error, setError] = useState();
+    const [ loading, setLoading ] = useState(true);
 
     useEffect(() => {
         axios.defaults.headers.common['Authorize'] = 'Bearer' + localStorage.getItem('token');
@@ -14,9 +16,11 @@ const Listusers = ({ role, user }) => {
         axios.get("/api/user/list-users")
             .then(res => {
                 setCustomers(res.data);
+                setLoading(false);
             })
             .catch(err => {
                 setError(err.response.data.errors[0].msg);
+                setLoading(false);
             })
     }, [])
 
@@ -40,6 +44,9 @@ const Listusers = ({ role, user }) => {
                         </li>
                     })}
                 </ul>
+                { loading && <div align="center">
+                    <img src={LoadingImg} width="120" height="120" alt="spinner"/>
+                    </div>}
                 { error && customers.length === 0 &&<div className={classes.noCustomers}>No customers found!</div>}
             </main>
         </React.Fragment>
