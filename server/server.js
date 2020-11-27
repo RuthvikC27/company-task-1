cd1db9290c3cc16debeb96be25b4bee287ec2610
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-11-27 12:52:13 +0530

    Initial

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = require("./src/express-conf");
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    mongoose.connect(`mongodb://127.0.0.1:27017/loan-app`, {
+    mongoose.connect(`mongodb://mongo:27017/loan-app`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
